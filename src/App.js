efb3e4fa1e623e1cee8afc0a17c9808b72b156ab
LuiZooKe <luizgustavodiascoro@gmail.com>
2025-05-24 12:32:38 -0300

    Limpando projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Routes/Home';
+import Home from './Routes/Home/Home';
 
 function App() {
   return (
